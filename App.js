a89f30903051a5f47ce4177fe400248bb0348ce9
Zakar Patrik <zakipepi@gmail.com>
2025-04-23 16:01:30 +0200

    Navigate to edit expense on list item press

--- App.js
+++ App.js
@@ -5,15 +5,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TabsScreen from './screens/TabsScreen';
 import ManageExpensesScreen from './screens/ManageExpensesScreen';
+import { GlobalStyles } from './constants/styles';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator id="stack-navigation" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator id="stack-navigation" screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: '#fff',
+        cardStyle: { backgroundColor: GlobalStyles.colors.primary800 },
+      }}>
         <Stack.Screen name="tab" component={TabsScreen} />
-        <Stack.Screen name="manage-expenses" component={ManageExpensesScreen} options={{ presentation: 'modal' }} />
+        <Stack.Screen name="manage-expenses" component={ManageExpensesScreen}
+                      options={{ headerShown: true, presentation: 'modal', headerLeft: () => <></> }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- components/ui/ListItem.jsx
+++ components/ui/ListItem.jsx
@@ -1,17 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
+import { useNavigation } from '@react-navigation/native';
 
 const ListItem = ({ description, date, amount }) => {
+  const navigation = useNavigation();
+
+  const editExpenseHandler = () => {
+    navigation.navigate('manage-expenses', { title: 'Edit Expense' });
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <View>
-        <Text style={styles.description}>{description}</Text>
-        <Text style={styles.date}>{date}</Text>
+    <Pressable android_ripple={{ color: '#fff' }} style={({ pressed }) => pressed ? styles.buttonPressed : null}
+               onPress={editExpenseHandler}>
+      <View style={styles.rootContainer}>
+        <View>
+          <Text style={styles.description}>{description}</Text>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <View style={styles.amountContainer}>
+          <Text style={styles.amountText}>{amount}</Text>
+        </View>
       </View>
-      <View style={styles.amountContainer}>
-        <Text style={styles.amountText}>{amount}</Text>
-      </View>
-    </View>
+    </Pressable>
   );
 };
 
@@ -49,5 +59,8 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: 'bold',
     color: GlobalStyles.colors.primary700,
+  },
+  buttonPressed: {
+    opacity: 0.8,
   },
 });
