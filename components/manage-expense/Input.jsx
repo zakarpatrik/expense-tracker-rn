3b89e4139215234deb66eb9b91f6e206e76a7459
Zakar Patrik <zakipepi@gmail.com>
2025-04-23 18:22:03 +0200

    Style the expense form

--- components/manage-expense/Input.jsx
+++ components/manage-expense/Input.jsx
@@ -1,12 +1,37 @@
-import { Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
 
-const Input = ({ label, config }) => {
+const Input = ({ label, style, config }) => {
+  const inputStyles = config.multiline ? [styles.input, styles.inputMultiline] : styles.input;
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...config} />
+    <View style={[styles.inputContainer, style]}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...config} />
     </View>
   );
 };
 
 export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.primary700,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
--- components/manage-expense/ManageExpenseForm.jsx
+++ components/manage-expense/ManageExpenseForm.jsx
@@ -1,4 +1,4 @@
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Input from './Input';
 
 const ManageExpenseForm = () => {
@@ -7,17 +7,20 @@ const ManageExpenseForm = () => {
   };
 
   return (
-    <View>
-      <Input label="Amount" config={{
-        keyboardType: 'decimal-pad',
-        onChangeText: amountChangeHandler,
-      }} />
-      <Input label="Date" config={{
-        placeholder: 'YYYY-MM-DD',
-        maxLength: 10,
-        onChangeText: () => {
-        },
-      }} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Your expense</Text>
+      <View style={styles.innerContainer}>
+        <Input label="Amount" style={styles.rowInput} config={{
+          keyboardType: 'decimal-pad',
+          onChangeText: amountChangeHandler,
+        }} />
+        <Input label="Date" style={styles.rowInput} config={{
+          placeholder: 'YYYY-MM-DD',
+          maxLength: 10,
+          onChangeText: () => {
+          },
+        }} />
+      </View>
       <Input label="Description" config={{
         multiline: true,
         autoCapitalize: 'none',
@@ -28,3 +31,24 @@ const ManageExpenseForm = () => {
 };
 
 export default ManageExpenseForm;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 40,
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
